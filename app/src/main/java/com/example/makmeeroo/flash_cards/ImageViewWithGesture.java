1504a62c0a8135f6e6e480749ef60dcb76f5e56f
vignesh-sethuraman <vignesh.sethuraman@gmail.com>
2017-01-02 11:03:53 -0800

    gestures reaching the class; need to program

--- app/src/main/java/com/example/makmeeroo/flash_cards/ImageViewWithGesture.java
+++ app/src/main/java/com/example/makmeeroo/flash_cards/ImageViewWithGesture.java
@@ -1,9 +1,11 @@
 package com.example.makmeeroo.flash_cards;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 /**
@@ -13,16 +15,53 @@ public class ImageViewWithGesture extends ImageView implements GestureDetector.O
 
     public ImageViewWithGesture(Context context) {
         super(context);
+        init(context);
     }
 
-//    public ImageViewWithGesture(Context context, AttributeSet attrs) {
-//        super(context, attrs);
-//    }
+    public ImageViewWithGesture(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public ImageViewWithGesture(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        // placeholder for init operations
+        setOnTouchListener(new View.OnTouchListener() {
+           @Override
+           public boolean onTouch(View v, MotionEvent event) {
+               // TODO
+               int action, pointerCount, i;
+               action = event.getAction();
+               pointerCount = event.getPointerCount();
+               Log.d("flash_cards", " imageviewwithGesture onTouch init called with action " + action + " pointerCount " + pointerCount);
+
+               switch (action) {
+                   case MotionEvent.ACTION_DOWN:
+                       return onDown(event);
+                       //break;
+
+                   case MotionEvent.ACTION_MOVE:
+                       //code here
+                       break;
+
+                   default:
+                       break;
+               }
+
+               return false;
+           }
+        }
+        );
+    }
 
     @Override
     public boolean onDown(MotionEvent e) {
         Log.d("flash_cards", " imageviewwithGesture onDown detected");
-        return false;
+        return true;
     }
 
     @Override
